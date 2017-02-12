4dbe6b0d4d7c93e2bbce38397b3b54639276cb29
xiaofeifei <liguoxin93@gmail.com>
2017-02-12 23:21:20 +0000

    add plot and practical 2.3.2

--- practical/practical2.3.R
+++ practical/practical2.3.R
@@ -1,27 +1,39 @@
+library(ggplot2)
+library(xtable)
 #load the data
-a=read.table("P:/R Data/beetlelarva.txt")
+a=read.table("/Users/xiaofeifei/Downloads/beetlelarva.txt")
 
 # change easting from factor to numerical data
-a$easting = as.numeric(a$easting)
+a$easting = factor(a$easting)
+a$northing = factor(a$northing)
+
 attach(a)
 
-#carry out a brief but careful prior elicitation leading to one or more priors for the parameters
-#prior
-x=seq(min(count),max(count),length.out=100)
-N=5
-v=9
-b.prior=matrix(rnorm(3*N,0,v),N,3) #simulate prior
-for (k in 2:(dim(b.prior)[1])) {
-  mu.sim=mu(a,b.prior[k,])
-  for (j in 1:48) {
-    y[j]=rpois(1,mu.sim[j])
-  }
-  lines (density(y))
-}
-plot(x,y,type='l',xlim=c(min(count),max(count)),ylim=c(0,1),xlab='scale(temp)',ylab='failure probability',main=paste('Variance',v))
-for (k in 2:N) {if (b.prior[k,2]>0) {y=logit(b.prior[k,],x); lines(x,y);}} #plot failure probability functions
+ggplot(data = a, aes(x = easting, y = northing)) +
+  geom_tile(aes(fill = count))+
+  xlab("Easting")+ ylab("Northing")+
+  ggtitle("Heat map of Japanese beetle larvae")
 
-# t (0,1), normal(0,1)
+sum_northing = aggregate(a$count, by=list(Category=a$northing), FUN=sum)
+sum_easting = aggregate(a$count, by=list(Category=a$easting), FUN=sum)
+
+#carry out a brief but careful prior elicitation leading to one or more priors for the parameters
+#Using simulation in prior elicitation
+K=1000
+n=length(a$count); mu.sima=matrix(NA,K,n)
+for (k in 1:K) {
+  lambda=rnorm(1,2,1) 
+  gamma=rnorm(12,0,1)            
+  beta = c(lambda,gamma)
+  mu.sim = mu(a,beta)
+  for (j in 1:n) {
+    mu.sima[k,j]=mu.sim[j]
+  }
+}
+par(mfrow = c(1,2))
+plot(apply(mu.sima,2,mean), xlab = "Each rectangle of fields", ylab = "simulated values",
+     main = "Check the normal prior by simulation")
+
 
 # Fit your models using MCMC, ABC or any other scheme that leads to a
 # Bayesian quantification of uncertainty
@@ -32,29 +44,28 @@ for (k in 2:N) {if (b.prior[k,2]>0) {y=logit(b.prior[k,],x); lines(x,y);}} #plot
 # Function to evaluate the likelihood for the glm L(beta;y) where beta=(beta[1],beta[2],beta[3]) 
 # are the GLM parameters for intercept, northing and easting. The likelihood is a product of Poisson's 
 # Let eta=beta[1]+beta[2]*northing+beta[2]*easting be the linear predictor so that mu(beta)=exp(eta(beta))
-
-mu<-function(a,beta) {
+X=model.matrix(~northing+easting,data = a)
+mu<-function(X,beta) {
   n=dim(a)[1]
-  X=cbind(rep(1,n), a$northing, a$easting)
   eta=X%*%beta
   return(exp(eta))    
 }
 
 #log likelihood poisson GLM
 llk<-function(a,beta) {
-  return(sum(log(dpois(a$count,mu(a,beta)))))
+  return(sum(dpois(a$count,mu(X,beta), log = T)))
 } 
 
 
 # use prior normal mean = 0, sd = 1
 lpr<-function(beta) {
-  sum(log(dt(beta,1)))
+  sum(dnorm(beta[2:13],0,1,log = T), dnorm(beta[1],1,1,log = T))
 }
 
 #MCMC setup
 
 #initialise 
-beta0=c(0,0,0)
+beta0=rep(0,13)
 
 
 #MCMC loop - here "beta" is the current state of the Markov chain.
@@ -64,7 +75,7 @@ MCMC<-function(K=10000,beta=c(0,0,0)) {
   #returns K samples from posterior using MCMC
   #no subsampling, start state goes in beta
   
-  B=matrix(NA,K,3); LP=rep(NA,K)
+  B=matrix(NA,K,13); LP=rep(NA,K)
   #storage
   
   lp=llk(a,beta)+lpr(beta) 
@@ -74,7 +85,10 @@ MCMC<-function(K=10000,beta=c(0,0,0)) {
     
     #tuned RW MH - adjusted the step sizes so they were 
     #unequal for beta[1] and beta[2] 
-    betap=beta+0.5*c(0.5,0.3,0.1)*rnorm(3) #generate candidate
+    ##########################################
+    ## why use different step size for beta ##
+    ##########################################
+    betap=beta+0.5*c(0.5,rep(0.1,12))*rnorm(13) #generate candidate
     
     lpp=llk(a,betap)+lpr(betap)        #calculate log post for candidate
     
@@ -92,16 +106,16 @@ MCMC<-function(K=10000,beta=c(0,0,0)) {
 }
 
 #basic plotting
-K=10000
+K=200000
 Output=MCMC(K,beta=beta0); 
 B=Output$B
 LP=Output$L
 
 #check for initial transient and drop it
+par(mfrow=c(1,2))
+plot(LP[1:400],type='l', xlab = "index of MCMC sample", ylab = "log posterior")
+plot(LP[seq(400,K,10)],xlab = "index of MCMC sample", ylab = "log posterior"); 
 
-# pdf('ps1q4bi.pdf',9,3)
-par(mfrow=c(1,2)); plot(LP[1:200],type='l'); plot(LP[seq(100,K,10)]); 
-# dev.off()
 #initial transient done by 100 steps - drop this burn-in
 LP<-LP[100:K]
 B<-B[100:K,]
@@ -109,19 +123,32 @@ B<-B[100:K,]
 #superpose histograms from multiple runs - 
 #looks OK they are consistent - here are histograms 
 #of the log-lkd from multiple runs change state
-plot(density(LP)); 
+beta1 = matrix(c(rep(1,13), rep(-1,13)),2,13, byrow = T)
+par(mfrow = c(1,1))
+plot(density(LP), xlab = "log posterior", main = ""); 
 for (i in 1:2) {
-  LP.check=MCMC(K,beta=beta0)$L[100:K]
-  lines(density(LP.check)); 
+  LP.check=MCMC(K,beta=beta1[i,])$L[100:K]
+  lines(density(LP.check), col=i+1); 
 }
 
 #alot of this is most easilly done using CODA
 #here are some obvious functions
+
+# 关于链的收敛有这样一些检验方法。 
+# （1）图形方法 这是简单直观的方法。我们可以利用这样一些图形：
+#  （a）迹图（trace plot）：将所产生的样本对迭代次数作图，生成马氏链的一条样本路径。如果当t足够大时，
+# 路径表现出稳定性没有明显的周期和趋势，就可以认为是收敛了。 
+# (b)自相关图（Autocorrelation plot）：如果产生的样本序列自相关程度很高，用迹图检验的效果会比较差。
+# 一般自相关随迭代步长的增加而减小，如果没有表现出这种现象，说明链的收敛性有问题。 
 library(coda); 
 HPDinterval(as.mcmc(B))
 effectiveSize(as.mcmc(B)) 
-plot(as.mcmc(B))
-acf(as.mcmc(B),lag.max=200)
+
+par(mfrow=c(3,1))
+plot(B[,3], type = "l", ylab = "estimated parameter value", xlab = "iteration", 
+     main = "Beta 3 trace plot")
+
+acf(as.mcmc(B[,1:3]),lag.max=3000)
 
 # you may be able to make a formal model comparison using Bayes factors or posterior odds, 
 # but a more qualitative comparison based for example on goodness of fit may also be acceptable
@@ -147,7 +174,7 @@ for (j in 1:n) {
 ecdf<-(table(c(count)))/sum(table(count))
 
 ecdf.mc<-(table(c(y)))/sum(table(y))
-
+scatterplot3d(b, type = "h", y.margin.add = 0.2, box = F)
 plot(ecdf.mc,type="h",lwd=5,xlab="number of bettle",
      ylab=expression(paste("Pr(",italic(Y[i]==y[i]),")",sep="")),col="gray",
      ylim=c(0,.35))
@@ -158,8 +185,9 @@ legend('topright',
          c("black","gray"),bty="n",cex=.8)
 
 #posterior predictive distribution of RSS/var
-#the variance looked a bit low so I compared the
-#ppd of (y-E(y))^2/E(y) with the value on the data
+#We are working with a Poisson, so I chose to check that predictive
+#distribution of the mean/variance ratio and compare it with
+#the value we got
 ########################################
 ## RSS/var, mean/var, (y-E(y))^2/E(y) ##
 ########################################
@@ -177,31 +205,45 @@ dv=mean(count)/var(count)
 abline(v=dv,col=2)
 mean(ts>dv) #no issues
 
-########################################################
-#Using simulation in prior elicitation Challanger/logistic example
-#######################
-#######################
-#######################
-ust=c(53,57,58,63,66,67,67,67,68,69,70,70,70,70,72,73,75,75,76,76,78,79,81)
-mean(ust)
-sd(ust)
+###############################
+b.p=MCMCprobit(fail~temp,b0=c(0,0),B0=c(3,3),seed=4,mcmc=N,marginal.likelihood='Laplace')
+b.l=MCMClogit(fail~temp,b0=c(0,0),B0=c(3,3),seed=5,mcmc=N,marginal.likelihood='Laplace')
 
-# pdf('logitpriorsample.pdf',12,6)
-par(mfrow=c(1,2))
-x=seq(min(count),max(count),length.out=100)
-N=100
-v=9
-b.prior=matrix(rnorm(3*N,0,v),N,3) #simulate prior
+logit<-function(beta,x=temp) {eta=beta[1]+beta[2]*x; exp(eta)/(1+exp(eta))}
+probit<-function(beta,x=temp) {eta=beta[1]+beta[2]*x; pnorm(eta)}
 
-plot(x,y,type='l',xlim=c(min(count),max(count)),ylim=c(0,1),xlab='scale(temp)',ylab='failure probability',main=paste('Variance',v))
-for (k in 2:N) {if (b.prior[k,2]>0) {y=logit(b.prior[k,],x); lines(x,y);}} #plot failure probability functions
+lkd<-function(beta,logit=T) {
+  if (logit) {pb=logit(beta)} else {pb=probit(beta)}
+  return(sum(dbinom(fail,1,pb,log=T)))
+}
 
-v=1
-b.prior=matrix(rnorm(2*N,0,v),N,2)
-b.prior[,2] = abs(b.prior[,2])
-y=logit(b.prior[1,],x)
-plot(x,y,type='l',xlim=c(min(temp),max(temp)),ylim=c(0,1),xlab='scale(temp)',ylab='failure probability',main=paste('Variance',v))
-for (k in 2:N) {if (b.prior[k,2]>0) {y=logit(b.prior[k,],x); lines(x,y);}}
-# dev.off()
+l.pr=l.lo=pr.lo=pr.pr=l.lo.pr=l.pr.lo=rep(NA,N)
+for (k in 1:N) {
+  l.lo[k]=lkd(b.l[k,],logit=T); 
+  l.pr[k]=lkd(b.p[k,],logit=F);
+  l.lo.pr[k]=lkd(b.p[k,],logit=T); 
+  l.pr.lo[k]=lkd(b.l[k,],logit=F);
+  pr.lo[k]=sum(dnorm(b.l[k,],0,9,log=T))
+  pr.pr[k]=sum(dnorm(b.p[k,],0,9,log=T))
+}
+
+#harmonic
+ml.lo=1/mean(1/exp(l.lo))
+ml.pr=1/mean(1/exp(l.pr))
+ml.lo/ml.pr
+
+#Bridge estimator just using h=1 - simple, performs well
+mean(exp(l.lo.pr)*pr.pr)/mean(exp(l.pr.lo)*pr.lo)
+
+#builtin function uses a Laplace estimator we didnt cover
+exp(attr(b.l,"logmarglike"))/exp(attr(b.p,"logmarglike"))
+
+#the naive estimator is hopeless
+N=1000000
+b.prior=matrix(rnorm(2*N,0,9),N,2)
+l.pr=l.lo=rep(NA,N)
+for (k in 1:N) {l.lo[k]=lkd(b.prior[k,],logit=T); l.pr[k]=lkd(b.prior[k,],logit=F)}
+mean(exp(l.lo))/mean(exp(l.pr))
+
 
 
