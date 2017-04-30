89a7f1a09b7877e6f3f9e26459b8c932efcd19b3
xiaofeifei1800 <liguoxin93@gmail.com>
2017-04-30 14:08:59 +0100

    organize code

--- practical/computational .R
+++ practical/computational .R
@@ -7,139 +7,105 @@ plot(y,xlab="Genome Index", ylab="Signal")
 # check NAs, total 30 NAs in y
 sum(is.na(y))
 
-# Exercise 1 (a)
-
-
 # since the observation data has missing values, for dealing with missing values,
 # I will partition the observation sequence into fully observed subsequences and 
 # then for each subsequence either pick the most likely next state and observation.
 
-alpha_recursion = function(y, mu, A, mean, sd)
+alpha_recursion = function(y, mu, A, mean, sd, t_dis)
 {
+  # y is the observed sequences
+  # mu is initial probability vector
+  # A is the transition matrix
+  # mean and sd are the emission probabilities
+  # t_dis logic value, switch between t density and normal density for class1
+  
+  # all available states
   K = length(mu)
-  T = length(y)
+  
+  # length of squences
+  Time = length(y)
+  
+  # define alpha matrix to store the alphe values
   alpha = matrix(0, nrow=T,ncol=K)
   
-  for (j in 1:K) 
+  # chcek which density will be used for class 1
+  if(t_dis == TRUE)
   {
-        alpha[1,j] = dnorm(y[1],mean[j], sd[j])*sum(A[,j]* mu)
-
-  }
-  
-  for (t in 2:T) 
-  {
-    if (is.na(y[t]))
+    # calculate the alpha at time 1
+    alpha[1,1] = (dt(y[1]/0.05,df = 4)/0.05)*sum(A[,1]* mu)
+    for (j in 2:K) 
     {
-      pred_x = numeric()
-      for (j in 1:K)
-      {
-        pred_x[j] = sum(A[,j]*(alpha[t-1,]/sum(alpha[t-1,])))
-        # pred_y = rnorm(1,mean[j], sd[j])
-      }
-      max_x = which.max(pred_x)
-      y[t] = rnorm(1,mean[max_x], sd[max_x])
-      assign("y", y, envir = .GlobalEnv) 
+      alpha[1,j] = dnorm(y[1],mean[j], sd[j])*sum(A[,j]* mu)
     }
-    for (j in 1:K)
-    {
-        alpha[t,j] = dnorm(y[t],mean[j], sd[j])*sum(A[,j]* alpha[t-1,])
-    }
-  }
-  return(alpha)
-}
-
-
-A = matrix(c(0.99, 0.005, 0.005, 0.005, 0.99, 0.005, 0.005, 0.005, 0.99),
-           nrow = 3, ncol = 3, byrow = T)
-
-mean = c(0,0.5,-0.5)
-sd = c(0.05, 0.08,0.08)
-mu = c(1/3,1/3,1/3)
-a = alpha_recursion(y, mu, A, mean, sd)
-
-
-beta_recursion = function(y, mu, A, mean, sd)
-{
-  K = length(mu)
-  T = length(y)
-  beta = matrix(0, nrow=T,ncol=K)
-  for (j in 1:K)
-  {
-    beta[T,j] = 1
-  }
-  
-  for (t in T:2)
-  {
-    for (i in 1:K)
-    {
-      B = c(dnorm(y[t],mean[1], sd[1]),
-            dnorm(y[t],mean[2], sd[2]),
-            dnorm(y[t],mean[3], sd[3]))
-      beta[t-1,i] = sum(B*A[i,]* beta[t,])
-    }
-  }
-  return(beta)
-}
-b = beta_recursion(y, mu, A, mean, sd)
-
-# smoothing, marginal maximum a posteriori
-smooth = a*b/apply(a*b, 1, sum)
-state = apply(smooth, 1, which.max)
-
-# Exercise 2
-# 2.1
-
-
-alpha_recursion = function(y, mu, A, mean, sd)
-{
-  K = length(mu)
-  T = length(y)
-  alpha = matrix(0, nrow=T,ncol=K)
-  alpha[1,1] = (dt(y[1]/0.05,df = 4)/0.05)*sum(A[,1]* mu)
-  for (j in 2:K) 
-  {
-    alpha[1,j] = dnorm(y[1],mean[j], sd[j])*sum(A[,j]* mu)
     
+  }else{
+    
+    for (j in 1:K) 
+    {
+      alpha[1,j] = dnorm(y[1],mean[j], sd[j])*sum(A[,j]* mu)
+    }
   }
   
-  for (t in 2:T) 
+  # start recursion 
+  for (t in 2:length) 
   {
+    # check if the observed state is missing value
+    # if is ture, will process forward prediction to estimated missing value
     if (is.na(y[t]))
     {
       pred_x = numeric()
       for (j in 1:K)
       {
         pred_x[j] = sum(A[,j]*(alpha[t-1,]/sum(alpha[t-1,])))
-        # pred_y = rnorm(1,mean[j], sd[j])
       }
+      
+      # find the most likely next state 
       max_x = which.max(pred_x)
+      
+      # simulate y 
       y[t] = rnorm(1,mean[max_x], sd[max_x])
+      
+      # overwrite the y
       assign("y", y, envir = .GlobalEnv) 
     }
-    alpha[t,1] = (dt(y[t]/0.05,df = 4)/0.05)*sum(A[,1]* alpha[t-1,])
-    for (j in 2:K)
+    
+    # chcek which density will be used for class 1
+    if(t_dis == TRUE)
     {
-      alpha[t,j] = dnorm(y[t],mean[j], sd[j])*sum(A[,j]* alpha[t-1,])
+      alpha[t,1] = (dt(y[t]/0.05,df = 4)/0.05)*sum(A[,1]* alpha[t-1,])
+      for (j in 2:K)
+      {
+        alpha[t,j] = dnorm(y[t],mean[j], sd[j])*sum(A[,j]* alpha[t-1,])
+      }
+    }else{
+      for (j in 1:K)
+      {
+        alpha[t,j] = dnorm(y[t],mean[j], sd[j])*sum(A[,j]* alpha[t-1,])
+      }
     }
   }
   return(alpha)
 }
 
-
-A = matrix(c(0.99, 0.005, 0.005, 0.005, 0.99, 0.005, 0.005, 0.005, 0.99),
-           nrow = 3, ncol = 3, byrow = T)
-
-mean = c(0,0.5,-0.5)
-sd = c(0.05, 0.08,0.08)
-mu = c(1/3,1/3,1/3)
-a = alpha_recursion(y, mu, A, mean, sd)
-
-
 beta_recursion = function(y, mu, A, mean, sd)
 {
+  # y is the observed sequences
+  # mu is initial probability vector
+  # A is the transition matrix
+  # mean and sd are the emission probabilities
+  # t_dis logic value, switch between t density and normal density for class1
+  
+  # all available states
   K = length(mu)
+  
+  # length of squences
   T = length(y)
+  
+  # define beta matrix to store the alphe values
   beta = matrix(0, nrow=T,ncol=K)
+  
+  # start recursion 
+  # calculate the beta at time T
   for (j in 1:K)
   {
     beta[T,j] = 1
@@ -149,25 +115,111 @@ beta_recursion = function(y, mu, A, mean, sd)
   {
     for (i in 1:K)
     {
-      B = c(dt(y[t]/0.05,df = 4)/0.05,
+      # chcek which density will be used for class 1
+      if(t_dis == TRUE)
+      {
+        B = c(dt(y[t]/0.05,df = 4)/0.05,
             dnorm(y[t],mean[2], sd[2]),
             dnorm(y[t],mean[3], sd[3]))
+        
+      }else{
+        
+        B = c(dnorm(y[t],mean[1], sd[1]),
+            dnorm(y[t],mean[2], sd[2]),
+            dnorm(y[t],mean[3], sd[3]))
+      }
       beta[t-1,i] = sum(B*A[i,]* beta[t,])
     }
   }
   return(beta)
 }
-b = beta_recursion(y, mu, A, mean, sd)
 
-# smoothing, marginal maximum a posteriori
-smooth = a*b/apply(a*b, 1, sum)
-state1 = apply(smooth, 1, which.max)
+# define transition matrix, emission probabilities, probability vector
+A = matrix(c(0.99, 0.005, 0.005, 0.005, 0.99, 0.005, 0.005, 0.005, 0.99),
+           nrow = 3, ncol = 3, byrow = T)
+mean = c(0,0.5,-0.5)
+sd = c(0.05, 0.08,0.08)
+mu = c(1/3,1/3,1/3)
+
+# run alpha_recursion and beta_recursion
+#normal density
+a_n = alpha_recursion(y, mu, A, mean, sd, FALSE)
+b_n = beta_recursion(y, mu, A, mean, sd,FALSE)
+
+# t density
+a_t = alpha_recursion(y, mu, A, mean, sd, TRUE)
+b_t = beta_recursion(y, mu, A, mean, sd, TRUE)
+# smoothing, find marginal maximum a posteriori
+smoothing_n = a_n*b_n/apply(a_n*b_n, 1, sum)
+estimate_state_n = apply(smoothing_n, 1, which.max)
+
+smoothing_t = a_t*b_t/apply(a_t*b_t, 1, sum)
+estimate_state_t = apply(smoothing_t, 1, which.max)
+
+# Define expectation-maximisation algorithm and estimate the transition matrix A
+EM = function(alpha, beta, A, mu, mean, sd)
+{
+  # alpha, beta are the result of the forward and backward recursions
+  # A is transition matrix
+  # mu is the initial probability vector
+  # mean and sd are the emission probabilities
+  
+  # all available states
+  K = length(mu)
+  # length of squences
+  Time = dim(alpha)[1]
+  
+  # define expected matrix to store the E[Ni,j |y1:T , A(k−1)]
+  E = matrix(0,nrow = 3, ncol = 3)
+  for(i in 1:K)
+  {
+    for(j in 1:K)
+    {
+      for(t in 2:Time)
+      {
+        # E[Ni,j |y1:T , A(k−1)]
+        E[i,j] = E[i,j] + alpha[t-1,i]*dnorm(y[t],mean[j], sd[j])*A[i,j]*beta[t,j]
+      }
+    }
+  }
+  
+  # Ai,j = E[Ni,j |y1:T , A(k−1)]/sum_l(E[Ni,l |y1:T , A(k−1)])
+  A = E/apply(E, 1,sum)
+  return(A)
+}
+
+# define transition matrix, emission probabilities, probability vector, likelihood
+A = matrix(rep(0.5,9), nrow = 3, ncol = 3, byrow = T)
+mean = c(0,0.5,-0.5)
+sd = c(0.05, 0.08,0.08)
+mu = c(1/3,1/3,1/3)
+likelihood = 0
+
+while(TRUE)
+{
+  # calculate forward and backward recursions
+  a = alpha_recursion(y, mu, A, mean, sd, FALSE)
+  b = beta_recursion(y, mu, A, mean, sd, FALSE)
+  
+  # calculate likelihood with new transition matrix
+  likelihoodnew = sum(a[dim(a)[1],])
+  if (likelihoodnew<likelihood)
+  {
+    break
+    
+  }else{
+    likelihood = likelihoodnew
+  }
+  
+  # apply EM algorithm
+  A = EM(a, b, A, mu, mean, sd)
+}
+
+###############################################################################
+###############################################################################
 
 # plot state and y
-returns.hv = as.data.frame(y)
-returns.hv$HighVolatility = state
-
-plot(y,xlab="Genome Index", ylab="Signal", col = state)
+plot(y,xlab="Genome Index", ylab="Signal", col = estimate_state)
 ind = which(state==1)
 points(x = ind,y = rep(0,length(ind)), col='red', pch=15)
 
@@ -177,7 +229,7 @@ points(x = ind,y = rep(0.5,length(ind)), col='green', pch=15)
 ind = which(state==3)
 points(x = ind,y = rep(-0.6,length(ind)), col='blue', pch=15)
 
-plot(y,xlab="Genome Index", ylab="Signal", col = state)
+plot(y,xlab="Genome Index", ylab="Signal", col = estimate_state1)
 ind = which(state1==1)
 points(x = ind,y = rep(0,length(ind)), col='red', pch=15)
 
@@ -196,77 +248,4 @@ qqline(y[state1==2])
 
 qqnorm(y[state1==3], main="Low volatility")
 qqline(y[state1==3])
-
-# EM
-A_inital = A = matrix(rep(0.5,9), nrow = 3, ncol = 3, byrow = T)
-mu = c(1/3,1/3,1/3)
-mean = c(0,0.5,-0.5)
-sd = c(0.05, 0.08,0.08)
-likelihood = 0
-for(i in 1:10)
-{
-  a = alpha_recursion(y, mu, A, mean, sd)
-  b = beta_recursion(y, mu, A, mean, sd)
-  
-  K = length(mu)
-  Time = length(y)
-  E = matrix(0,nrow = 3, ncol = 3)
-  for(i in 1:K)
-  {
-    for(j in 1:K)
-    {
-      for(t in 2:Time)
-      {
-        E[i,j] = E[i,j] + a[t-1,i]*dnorm(y[t],mean[j], sd[j])*A[i,j]*b[t,j]
-      }
-    }
-  }
-  
-  A = E/apply(E, 1,sum)
-  likelihoodnew = sum(a[dim(a)[1],])
-  if (likelihoodnew<likelihood)
-  {
-    break
-  }
-}
-
-#############################################################################
-
-A_inital = A = matrix(rep(0.5,9), nrow = 3, ncol = 3, byrow = T)
-mu = c(1/3,1/3,1/3)
-mean = c(0,0.5,-0.5)
-sd = c(0.05, 0.08,0.08)
-likelihood = 0
-for(i in 1:10)
-{
-  a = alpha_recursion(y, mu, A, mean, sd)
-  b = beta_recursion(y, mu, A, mean, sd)
-  
-  K = length(mu)
-  Time = length(y)
-  E = matrix(0,nrow = 3, ncol = 3)
-  for(i in 1:K)
-  {
-    for(j in 1:K)
-    {
-      for(t in 2:Time)
-      {
-        if (j == 1)
-        {
-          E[i,j] = E[i,j] + a[t-1,i]*(dt(y[t]/0.05,df = 4)/0.05)*A[i,j]*b[t,j]
-        }else{
-          E[i,j] = E[i,j] + a[t-1,i]*dnorm(y[t],mean[j], sd[j])*A[i,j]*b[t,j]
-        }
-      }
-    }
-  }
-  
-  A = E/apply(E, 1,sum)
-  likelihoodnew = sum(a[dim(a)[1],])
-  if (likelihoodnew<likelihood)
-  {
-    break
-  }
-}
-
 
--- practical/statistical programing.R
+++ practical/statistical programing.R
@@ -120,39 +120,82 @@ stopCluster(cl)
 
 
 #######################################################################
+library(penalized)
+library(doParallel)
+data("nki70")
+attach(nki70)
 
-lambda1 = 10
-lambda2 = 10
-stepping1 = 1
-stepping2 = 1
+#Input: 
 
-
-lambda11 = append(lambda1, c(lambda1+stepping1, lambda1-stepping1))
-lambda22 = append(lambda2, c(lambda2+stepping2, lambda2-stepping2))
-
-all_models = expand.grid(lambda11,lambda22)
-
-cl = makeCluster(4)
-clusterExport(cl, list("nki70"))
-clusterEvalQ(cl, library(penalized))
-
-all_results = numeric()
-system.time(
-  for(i in 1:9)
-  {
-    result = parLapply(cl, 1:10, parallel.xval,lambda1 = all_models[i,1],lambda2=all_models[i,2])
-    all_results[i]= mean(unlist(result))
-  }
-)
-stopCluster(cl)
-
-parallel.xval = function(data, lambda1, lambda2)
+parallel.xval = function(index, lambda1, lambda2)
 {
   fit=cvl(nki70$time, nki70[, 8:77], lambda1 = lambda1, lambda2 =lambda2, fold = 10,
           model = "linear")
   corr = cor(nki70$time, fit$predictions[,1])
   return(corr)
 }
+
+grid_search = function(lambda1, lambda2, stepping1, stepping2, epsilon )
+{
+  
+  result = parLapply(cl, 1:10, parallel.xval,lambda1 = lambda1,lambda2=lambda1)
+  base_pred_cor = mean(unlist(result))
+
+
+  lambda1_new = append(lambda1, c(lambda1+stepping1, lambda1-stepping1))
+  lambda2_new = append(lambda2, c(lambda2+stepping2, lambda2-stepping2))
+  
+  all_models = expand.grid(lambda1_new,lambda2_new)
+  
+  predict_cor = numeric()
+  
+  while(TRUE)
+  {
+    for(i in 1:9)
+    {
+      result = parLapply(cl, 1:10, parallel.xval,lambda1 = all_models[i,1],lambda2=all_models[i,2])
+      predict_cor[i]= mean(unlist(result))
+    }
+  
+    if(any(predict_cor-base_pred_cor>epsilon))
+    {
+      lambda1 = as.numeric(all_models[which.max(predict_cor),][1,1])
+      lambda2 = as.numeric(all_models[which.max(predict_cor),][1,2])
+      base_pred_cor = max(predict_cor)
+    }else{
+      lambda1 = lambda1
+      lambda2 = lambda2
+      break
+    }
+    
+    if(lambda1 < stepping1)
+    {
+      stepping1 = lambda1/2
+    }
+    
+    if(lambda2 < stepping2)
+    {
+      stepping2 = lambda2/2
+    }
+  }  
+  return(list(lambda1, lambda2, max(predict_cor)))
+}
+
+cl <- makeCluster(detectCores()-1)
+registerDoParallel(cl)
+clusterExport(cl, list("nki70"))
+clusterEvalQ(cl, library(penalized))
+
+lambda1 = 20
+lambda2 = 20
+stepping1 = 5
+stepping2 = 5
+epsilon = 0.001
+
+grid_search(lambda1, lambda2, stepping1, stepping2, epsilon )
+
+stopCluster(cl)
+
 
 
 ##############################################################
